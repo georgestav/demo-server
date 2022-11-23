2b78681b67844e1b1c9dcc7268202cb6a317a90b
George Stavroulakis <g.stavroulakis@gmail.com>
2022-11-23 08:23:25 +0100

    get student

--- src/main/java/com/codealpine/serverdemo/ServerDemoApplication.java
+++ src/main/java/com/codealpine/serverdemo/ServerDemoApplication.java
@@ -1,10 +1,18 @@
 package com.codealpine.serverdemo;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codealpine.serverdemo.student.Student;
 
 @SpringBootApplication
+@RestController
 public class ServerDemoApplication {
 
 	public static void main(String[] args) {
@@ -12,8 +20,8 @@ public class ServerDemoApplication {
 	}
 
 	@GetMapping
-	public static String homepage() {
-		return "testing";
+	public List<Student> homepage() {
+		return List.of(new Student(1L, "IamAstudent", "example@example.com", LocalDate.of(2000, Month.APRIL, 5), 22));
 	}
 
 }
